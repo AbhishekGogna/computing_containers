275df1a86800957352acd5a83a28658a127ad86d
abhi <abhishekgogana@gmail.com>
2023-03-23 10:56:07 +0100

    fixed issue of installing specific libraries for R tidyverse

--- usr_scr/install_pkgs.R
+++ usr_scr/install_pkgs.R
@@ -3,9 +3,9 @@ args = commandArgs(trailingOnly=TRUE)
 
 options(Ncpus = args[1])
 ### Utilities for R Jupyter Kernel
-install.packages(c("base64enc","evaluate","IRdisplay","jsonlite","uuid","digest"), repos="http://cran.us.r-project.org", dependencies=TRUE)
+install.packages(c("base64enc","evaluate","jsonlite","uuid","digest"), repos="http://cran.us.r-project.org", dependencies=TRUE, quite = TRUE)
 ### Install R Jupyter Kernel
-install.packages(c("repr", "IRdisplay", "crayon", "pbdZMQ", "devtools"),repos="http://cran.us.r-project.org", dependencies=TRUE)
+install.packages(c("repr", "IRdisplay", "crayon", "pbdZMQ", "devtools", "renv"),repos="http://cran.us.r-project.org", dependencies=TRUE, quite= TRUE)
 devtools::install_github("IRkernel/IRkernel")
 ### Install R kernel
 IRkernel::installspec(user = FALSE, name = "ir405", displayname = "R 4.0.5")
