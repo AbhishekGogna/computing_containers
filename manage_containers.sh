94b335285ce0186516358905a2f6e687c604256b
Abhishek Gogna <gogna@qg-10.ipk-gatersleben.de>
2023-03-23 15:08:45 +0100

    minor change to varaible expansion

--- manage_containers.sh
+++ manage_containers.sh
@@ -134,8 +134,8 @@ start_jupyter(){
  		--pwd "/proj/" \
  		"instance://${ins_name}" \
  		jupyter lab \
-        --ip=${address} \
-        --port=${port} \
+        --ip="${address}" \
+        --port="${port}" \
 		--ServerApp.root_dir="/proj/" \
 		> "${logs}" \
 		2> "${err}") &
